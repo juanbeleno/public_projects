76f0f35b2c65325d49fef3c1bef7b5184f9bf793
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2022-10-24 23:07:20 -0500

    Training many tickets

--- day_trading/day_trading/dataset.py
+++ day_trading/day_trading/dataset.py
@@ -14,66 +14,54 @@ import requests
 
 class DayTradingDataset:
     def __init__(self) -> None:
-        self.tickets = [
-            'AAPL',
-            'NVDA',
-            'TSLA',
-            'GOOG',
-            'META',
-            'MSFT',
-            'QQQ',
-            'SPY'
-        ]
         self.range = '60d'
         self.granularity = '5m'
         self.stop_intervals = 8
 
-    def get_raw_data(self):
+    def get_raw_data(self, ticket):
         response = []
-        for ticket in self.tickets:
-            print(f'Getting data for {ticket}')
-            # Get the data from the past stored manually by me (Juan Beleño)
-            # collecting the data on certain dates.
-            files = DayTradingFiles()
-            ticket_directory = os.path.join(files.input_directory, f'{ticket}_interval_{self.granularity}_range_{self.range}')
-            filepaths = next(os.walk(ticket_directory), (None, None, []))[2]  # [] if no file
+        print(f'Getting data for {ticket}')
+        # Get the data from the past stored manually by me (Juan Beleño)
+        # collecting the data on certain dates.
+        files = DayTradingFiles()
+        ticket_directory = os.path.join(files.input_directory, f'{ticket}_interval_{self.granularity}_range_{self.range}')
+        filepaths = next(os.walk(ticket_directory), (None, None, []))[2]  # [] if no file
 
-            for filepath in filepaths:
-                with open(os.path.join(ticket_directory, filepath)) as json_file:
-                    response.append(json.load(json_file))
+        for filepath in filepaths:
+            with open(os.path.join(ticket_directory, filepath)) as json_file:
+                response.append(json.load(json_file))
 
-            # Collect the most recent data using the Yahoo Finances API
-            params = {
-                'region': 'US',
-                'lang': 'en-US',
-                'includePrePost': 'false',
-                'interval': self.granularity,
-                'useYfid': 'true',
-                'range': self.range,
-                'corsDomain': 'finance.yahoo.com',
-                '.tsrc': 'finance'
-            }
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
-            }
-            request = requests.get(
-                f'https://query1.finance.yahoo.com/v8/finance/chart/{ticket}',
-                params=params,
-                headers=headers
-            )
-            recent_data = request.json()
-            response.append(recent_data)
+        # Collect the most recent data using the Yahoo Finances API
+        params = {
+            'region': 'US',
+            'lang': 'en-US',
+            'includePrePost': 'false',
+            'interval': self.granularity,
+            'useYfid': 'true',
+            'range': self.range,
+            'corsDomain': 'finance.yahoo.com',
+            '.tsrc': 'finance'
+        }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
+        }
+        request = requests.get(
+            f'https://query1.finance.yahoo.com/v8/finance/chart/{ticket}',
+            params=params,
+            headers=headers
+        )
+        recent_data = request.json()
+        response.append(recent_data)
 
         return response
 
-    def prepare_dataset(self):
+    def prepare_dataset(self, ticket):
         print('Getting the data for the tickets')
-        raw_data = self.get_raw_data()
+        raw_data = self.get_raw_data(ticket)
 
         print('Convert the HTTP response into a pandas DataFrame')
-        dataset = pd.DataFrame(columns=['timestamp', 'high', 'low', 'close', 'open', 'volume', 'ticket'])
+        dataset = pd.DataFrame(columns=['timestamp', 'high', 'low', 'close', 'open', 'volume'])
         for partial_data in raw_data:
-            ticket = partial_data['chart']['result'][0]['meta']['symbol']
             print(f'Processing data for {ticket}')
             timestamps = partial_data['chart']['result'][0]['timestamp']
             data = partial_data['chart']['result'][0]['indicators']['quote'][0]
@@ -85,17 +73,10 @@ class DayTradingDataset:
                 'open': data['open'],
                 'volume': data['volume']
             })
-            partial_dataset['ticket'] = ticket
             dataset = pd.concat([partial_dataset, dataset])
 
         # Remove duplicated data
         dataset.drop_duplicates(inplace=True)
-
-        # Use only one ticket
-        dataset = dataset[dataset['ticket'] == 'AAPL'].copy()
-
-        # Create binary column with the values of each ticket
-        dataset = pd.get_dummies(dataset, columns=['ticket'])
 
         # Convert int to timestamp
         dataset['timestamp'] = dataset['timestamp'].apply(
@@ -137,8 +118,8 @@ class DayTradingDataset:
 
         return dataset
 
-    def test_train_split(self):
-        dataset = self.prepare_dataset()
+    def test_train_split(self, ticket):
+        dataset = self.prepare_dataset(ticket)
 
         # Define timestamp ranges for datasets
         week_ago = datetime.now() - timedelta(days=7)
@@ -150,8 +131,8 @@ class DayTradingDataset:
 
         print('Defining the training data.')
         train_df = dataset[dataset['timestamp'] <= week_ago].copy()
-        files = DayTradingFiles()
-        train_df.to_csv(files.train_data_filepath, index=False)
+        # files = DayTradingFiles()
+        # train_df.to_csv(files.train_data_filepath, index=False)
         # Sample the dataset to add a little bit of randomness before training
         train_df = train_df.sample(frac=1, ignore_index=True)
         target_high_train_df = train_df['target_high'].tolist()
@@ -159,7 +140,7 @@ class DayTradingDataset:
         features_train_df = train_df[features].copy()
 
         print('Defining the test data.')
-        test_df = dataset[((dataset['timestamp'] >= week_ago) & (dataset['ticket_AAPL'] == 1))].copy()
+        test_df = dataset[dataset['timestamp'] >= week_ago].copy()
         test_df.dropna(subset=['target_high', 'target_low'], inplace=True)
         target_high_test_df = test_df['target_high'].tolist()
         target_low_test_df = test_df['target_low'].tolist()
--- day_trading/day_trading/train.py
+++ day_trading/day_trading/train.py
@@ -10,6 +10,7 @@ from xgboost import XGBRegressor
 from sklearn.linear_model import LinearRegression
 import joblib
 import json
+import os
 
 class DayTradingTrainer:
     def __init__(self) -> None:
@@ -18,24 +19,35 @@ class DayTradingTrainer:
         # self.low_regressor = XGBRegressor()
         self.high_regressor = LinearRegression()
         self.low_regressor = LinearRegression()
+        self.tickets = [
+            'AAPL',
+            'NVDA',
+            'TSLA',
+            'GOOG',
+            'META',
+            'MSFT',
+            'QQQ',
+            'SPY'
+        ]
 
     def train(self):
-        # Load the dataset
-        (
-            features_train_df, target_high_train_df, target_low_train_df,
-            features_test_df, target_high_test_df, target_low_test_df,
-        ) = self.day_trading_dataset.test_train_split()
+        for ticket in self.tickets:
+            # Load the dataset
+            (
+                features_train_df, target_high_train_df, target_low_train_df,
+                features_test_df, target_high_test_df, target_low_test_df,
+            ) = self.day_trading_dataset.test_train_split(ticket)
 
-        print('Training the models.')
-        self.high_regressor.fit(features_train_df, target_high_train_df)
-        self.low_regressor.fit(features_train_df, target_low_train_df)
+            print('Training the models.')
+            self.high_regressor.fit(features_train_df, target_high_train_df)
+            self.low_regressor.fit(features_train_df, target_low_train_df)
 
-        print('Saving the models and the test datasets.')
-        files = DayTradingFiles()
-        joblib.dump(self.low_regressor, files.low_model_filepath)
-        joblib.dump(self.high_regressor, files.high_model_filepath)
-        features_test_df.to_csv(files.features_filepath, index=False)
-        with open(files.target_high_filepath, 'w') as f:
-            json.dump(target_high_test_df, f)
-        with open(files.target_low_filepath, 'w') as f:
-            json.dump(target_low_test_df, f)
+            print('Saving the models and the test datasets.')
+            files = DayTradingFiles()
+            joblib.dump(self.low_regressor, os.path.join(files.output_directory, f'low_model_{ticket}.joblib'))
+            joblib.dump(self.high_regressor, os.path.join(files.output_directory, f'high_model_{ticket}.joblib'))
+            features_test_df.to_csv(os.path.join(files.output_directory, f'features_{ticket}.csv'), index=False)
+            with open(os.path.join(files.output_directory, f'target_high_{ticket}.json'), 'w') as f:
+                json.dump(target_high_test_df, f)
+            with open(os.path.join(files.output_directory, f'target_low_{ticket}.json'), 'w') as f:
+                json.dump(target_low_test_df, f)
